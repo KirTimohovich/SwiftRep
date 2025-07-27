64062be8d3467b1c3b2b97a5de3b6c358b6915c6
KirTimohovich <kirilltimtk@gmail.com>
2025-07-27 13:21:38 +0300

    HW_screens

---
+++ ThreeScreens/ThreeScreens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreeScreens
+//
+//  Created by Kiryl on 27.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
